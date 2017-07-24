8aa4bdd8170441698d4ee47229c025b6f99672c6
JEFFREY-PC\hinacom <jasonliuspark@outlook.com>
2017-07-24 13:59:50 +0800

    initial

--- trans/Form1.cs
+++ trans/Form1.cs
@@ -12,9 +12,27 @@ namespace trans
 {
     public partial class Form1 : Form
     {
+        static string excel_filename;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Open_Excel(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Multiselect = true;
+            openfile.Title = "please choose the excel file";
+            openfile.Filter = "(*.xls)|*.xls";
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+
+                excel_filename = openfile.FileName;
+      
+                
+
+            }
+
         }
     }
 }
